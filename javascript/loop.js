0ffa0867b562bf6a0880145ae39c337e662b0350
nasrin <nasrin.heritage@gmail.com>
2024-02-13 20:53:20 +0530

    add all files

---
+++ javascript/loop.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <= 20; i++) {
+    console.log(`outer loop value: ${i}`);
+    for (let j=1;j<=20;j++) {
+        console.log(i + '*' + j+' = ' + i*j );
+        
+        
+    }
+    
+}
